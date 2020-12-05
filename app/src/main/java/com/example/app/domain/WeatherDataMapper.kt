d3c75e0a25ee7afc51c73b995fdddc85d417e9ed
adamkis <kisferencadam@gmail.com>
2020-12-05 14:50:04 +0100

    fixed line length

--- app/src/main/java/com/example/app/domain/WeatherDataMapper.kt
+++ app/src/main/java/com/example/app/domain/WeatherDataMapper.kt
@@ -13,7 +13,8 @@ class WeatherDataMapper @Inject constructor(private val context: Context) {
     fun map(weatherDataResponse: WeatherDataResponse): WeatherVM {
         val weatherSource = weatherDataResponse.consolidatedWeather?.get(0)
         return WeatherVM(
-                icon = "https://www.metaweather.com/static/img/weather/png/${weatherSource?.weatherStateAbbr}.png",
+                icon = "https://www.metaweather.com/static/img/weather/png/" +
+                        "${weatherSource?.weatherStateAbbr}.png",
                 temp = context.getString(R.string.temp_in_celsius, weatherSource?.theTemp),
                 description = weatherSource?.weatherStateName ?: "",
                 dateTime = weatherDataResponse.time
